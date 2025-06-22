b1b22e6599a82758d419a16b6df148887e9c8c97
LukasKahmann <kahmann.lukas@gmail.com>
2025-06-22 11:01:01 +0200

    change includes to relative

--- js/includes.js
+++ js/includes.js
@@ -1,13 +1,10 @@
-// Repo-Name anpassen, falls nötig!
-const repoRoot = '/portfolio/';
-
 async function includeHTML() {
   // Header
   const headerEl = document.getElementById('header');
   if (headerEl) {
-    let headerFile = repoRoot + 'partials/header.html';
+    let headerFile = "partials/header.html";
     if (window.location.pathname.includes('/projects/')) {
-      headerFile = repoRoot + 'partials/header-projects.html';
+      headerFile = "../partials/header-projects.html";
     }
     const resp = await fetch(headerFile);
     headerEl.innerHTML = await resp.text();
@@ -15,7 +12,10 @@ async function includeHTML() {
   // Footer
   const footerEl = document.getElementById('footer');
   if (footerEl) {
-    const footerFile = repoRoot + 'partials/footer.html';
+    let footerFile = "partials/footer.html";
+    if (window.location.pathname.includes('/projects/')) {
+      footerFile = "../partials/footer.html";
+    }
     const resp = await fetch(footerFile);
     footerEl.innerHTML = await resp.text();
   }
