8f88cd98acf031083eb1deffe967311877d6c943
LukasKahmann <kahmann.lukas@gmail.com>
2025-06-22 10:47:05 +0200

    add absoulte path to all sites

--- js/includes.js
+++ js/includes.js
@@ -1,21 +1,21 @@
+// Repo-Name anpassen, falls nötig!
+const repoRoot = '/portfolio/';
+
 async function includeHTML() {
   // Header
   const headerEl = document.getElementById('header');
   if (headerEl) {
-    let headerFile = 'partials/header.html';
+    let headerFile = repoRoot + 'partials/header.html';
     if (window.location.pathname.includes('/projects/')) {
-      headerFile = '../partials/header-projects.html';
+      headerFile = repoRoot + 'partials/header-projects.html';
     }
     const resp = await fetch(headerFile);
     headerEl.innerHTML = await resp.text();
   }
-  // Footer (bleibt identisch)
+  // Footer
   const footerEl = document.getElementById('footer');
   if (footerEl) {
-    let footerFile = 'partials/footer.html';
-    if (window.location.pathname.includes('/projects/')) {
-      footerFile = '../partials/footer.html';
-    }
+    const footerFile = repoRoot + 'partials/footer.html';
     const resp = await fetch(footerFile);
     footerEl.innerHTML = await resp.text();
   }
